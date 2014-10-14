facefbd835725c0af14e09182471c84426cc2b01
Tokhir Dadaev <zx8754@gmail.com>
2014-10-14 10:11:33 +0100

    Create sampleLocation.R

---
+++ sampleLocation.R
@@ -0,0 +1,57 @@
+sampleLocation <- function(targetSamples,SampleLocRaw="Path:/to/progeny/DNA/output.txt" )
+{
+  SampleLocRaw <- read.delim(SampleLocRaw, na.strings=c("NA","","./."), as.is=T)
+  
+  #Filter ALL sample by target samples
+  SampleLocRaw <- SampleLocRaw[ SampleLocRaw$Study.ID %in% targetSamples$Study.ID, ]
+  
+  #vector of input samples
+  VecIn <- unique(SampleLocRaw$Study.ID)
+  
+  #filter available samples
+  SampleAvail <- SampleLocRaw[SampleLocRaw$DNA.Status=="Available",]
+  
+  #vector of input samples
+  VecAvail <- unique(SampleAvail$Study.ID)
+  
+  #Make df of unavailable samples --------
+  #Clean variables
+  temp=NULL
+  SampleUnavail=NULL
+  
+  for(i in c(setdiff(VecIn, VecAvail))) {
+    temp <- SampleLocRaw[SampleLocRaw$Study.ID == i,]; 
+    SampleUnavail <- rbind(SampleUnavail, temp)
+  }
+  
+  rm(temp)
+  
+  
+  #Combine avail and unavail
+  SampleLoc <- rbind(SampleAvail, SampleUnavail)
+  
+  
+  #Test custom sort -------
+  #http://stackoverflow.com/questions/1568511/how-do-i-sort-one-vector-based-on-values-of-another
+  #testOrder <- c("Primary", "Secondary", "Stock")
+  #testOrder[order(match(testOrder,SamOrder))]
+  #[1] "Secondary" "Primary"   "Stock"  
+  
+  #set sample preference left -> right
+  SamOrder <- c("Secondary", "Primary", "Stock", "Surplus", "Dilution")
+  
+  selectvec <- sapply(unique(SampleLoc$Study.ID), function(x) {
+    #create vector of row.namefor sample "x"
+    unsorted <- row.names(SampleLoc)[SampleLoc$Study.ID %in% x]
+    
+    #order vector by sample preference and take first row.name from vector
+    unsorted[order(match(t(as.data.frame(SampleLoc[unsorted, "Plate.Type"], row.names=unsorted, stringsAsFactors=F)), SamOrder))][1]
+  })
+  
+  SamLocOutput <- SampleLoc[selectvec,]
+  
+  #sort by pos and plate
+  SamLocOutput <- SamLocOutput[order(SamLocOutput$Position),]
+  SamLocOutput <- SamLocOutput[order(SamLocOutput$Plate.ID),]
+  return(SamLocOutput)
+}
