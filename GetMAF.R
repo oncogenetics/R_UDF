b75265e64aeb78f436a1210b700d8c7f9da8a7f4
Tokhir Dadaev <zx8754@gmail.com>
2015-05-22 14:52:39 +0100

    Update GetMAF.R

--- GetMAF.R
+++ GetMAF.R
@@ -12,6 +12,10 @@ udf_getMAF<-function(Z,missing=9){
   # MAF = mean divided by 2.
   maf<-colMeans(Z,na.rm = TRUE)/2
   
+  #if flipped mark it
+  maf <- cbind(ifelse(maf>0.5,1-0.5,maf),
+               ifelse(maf>0.5,1,0))
+
   #return MAF
   return(maf)
 }
